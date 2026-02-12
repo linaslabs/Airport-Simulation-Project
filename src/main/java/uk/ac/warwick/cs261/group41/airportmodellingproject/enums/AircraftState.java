62d5438fe1379d13863ecc0cacf82f7f339c2d83
Josh Gray <joshuagray495@gmail.com>
2026-02-12 01:10:43 +0000

    Added the basic DTOs and Enums required for the first prototype.

---
+++ src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/enums/AircraftState.java
@@ -0,0 +1,10 @@
+package uk.ac.warwick.cs261.group41.airportmodellingproject.enums;
+
+public enum AircraftState {
+    QUEUED_FOR_LANDING,
+    QUEUED_FOR_TAKEOFF,
+    DIVERTED,
+    CANCELLED,
+    LANDED,
+    TAKEN_OFF
+}
---
+++ src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/enums/RunwayStatus.java
@@ -0,0 +1,8 @@
+package uk.ac.warwick.cs261.group41.airportmodellingproject.enums;
+
+public enum RunwayStatus {
+    AVAILABLE,
+    INSPECTION,
+    SNOWCLEARANCE,
+    FAILURE
+}
