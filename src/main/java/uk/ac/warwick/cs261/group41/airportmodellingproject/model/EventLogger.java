babe139edc17457d2913a782f34a0dedffe3397d
Abel Linas <linasabel@gmail.com>
2026-02-17 17:46:12 +0000

    Moved EventLogger and EventManager to service package

--- src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/model/EventLogger.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.warwick.cs261.group41.airportmodellingproject.model;
-
-public class EventLogger {
-
-    public EventLogger() {}
-}
---
+++ src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/service/EventManager.java
@@ -0,0 +1,6 @@
+package uk.ac.warwick.cs261.group41.airportmodellingproject.service;
+
+public class EventManager {
+
+    public EventManager() {}
+}
