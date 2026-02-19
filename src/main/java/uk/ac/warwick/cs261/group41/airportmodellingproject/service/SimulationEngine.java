239ebbcf6c76c2d163826df5b38e3debc97c914d
Abel Linas <linasabel@gmail.com>
2026-02-19 10:48:57 +0000

    Further engine work

--- src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/service/SimulationEngine.java
+++ src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/service/SimulationEngine.java
@@ -2,15 +2,11 @@ package uk.ac.warwick.cs261.group41.airportmodellingproject.service;
 
 import uk.ac.warwick.cs261.group41.airportmodellingproject.dto.RunwayConfig;
 import uk.ac.warwick.cs261.group41.airportmodellingproject.dto.SimulationConfig;
-import uk.ac.warwick.cs261.group41.airportmodellingproject.model.Airport;
-import uk.ac.warwick.cs261.group41.airportmodellingproject.model.Runway;
-import uk.ac.warwick.cs261.group41.airportmodellingproject.model.StatisticsGenerator;
-import uk.ac.warwick.cs261.group41.airportmodellingproject.model.TakeOffQueue;
+import uk.ac.warwick.cs261.group41.airportmodellingproject.dto.SimulationProgress;
+import uk.ac.warwick.cs261.group41.airportmodellingproject.dto.StatisticsSummary;
+import uk.ac.warwick.cs261.group41.airportmodellingproject.model.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SimulationEngine {
 
@@ -19,7 +15,7 @@ public class SimulationEngine {
     private AircraftGenerator generator;
     private EventManager manager;
     private EventLogger eventLogger;
-    private StatisticsGenerator statisticsGenerator;
+    private Statistics stats;
     private int currentTick;
     private int durationTicks;
 
@@ -30,30 +26,75 @@ public class SimulationEngine {
 
     public void initialiseSimulation() {
 
-        this.statisticsGenerator = new StatisticsGenerator();
+        this.stats = new Statistics();
 
+        Random random = new Random(config.getSeed());
         // Pass maxWaitTime, runwaySettings, and Statistics to Airport
         // Its constructor will instantiate the queues and the runways
-        this.airport = new Airport();
+        this.airport = new Airport("SimulationAirport", this.config.getRunwaySettings(), this.config.getMaxWaitTime(), 100);
 
         this.eventLogger = new EventLogger();
-        // Pass EventLogger, Airport and Statistics into the EventManager
+        // Pass EventLogger, Airport and Statistics and random into the EventManager
         this.manager = new EventManager();
 
         // Instantiate AircraftGenerator, pass it the random seed
+        this.generator = new AircraftGenerator(random, config.getInboundRate(), config.getOutboundRate(), "SimulationAirport");
 
     }
 
     public boolean performTick() {
-        return true;
+        // Call event managers process scheduled events
+
+        // Get all inbound aircraft to be generated in the current tick and accept inbound into airport
+        List<Aircraft> inboundAircraft = this.generator.getInboundForTick(this.currentTick);
+        for (Aircraft aircraft : inboundAircraft) {
+            this.airport.acceptInboundAircraft(aircraft);
+        }
+
+        // Get all outbound aircraft to be generated in the current tick and accept outbound into airport
+        List<Aircraft> outboundAircraft = this.generator.getInboundForTick(this.currentTick);
+        for (Aircraft aircraft : outboundAircraft) {
+            this.airport.acceptOutboundAircraft(aircraft);
+        }
+
+        // Assign airport runways
+        this.airport.assignRunways(this.currentTick);
+
+        // Update airport queues
+        this.airport.updateQueues(this.currentTick);
+
+        // Increment to next tick
+        this.currentTick++;
+
+        // If current tick is equal to the end tick, return false (no longer continue ticks)
+        return this.currentTick != this.durationTicks;
+
     }
 
-    public double getProgress() {
-        // This will be used for the simple real-time progress update view.
-        return 0.0;
+    public SimulationProgress getSimulationProgress() {
+        return new SimulationProgress((double) this.currentTick / this.durationTicks);
     }
 
-    public void stop() {
-        // Handles simulation shutdown logic.
-    }
+    // Currently not to be used, getSimulationProgress takes its place for initial sprints
+//    public StatisticsSummary getFinalSummary() {
+//        return null;
+//    }
+
+
+//    public List<SimulationEvent> getEventLog() {
+//
+//    }
+
+    public SimulationConfig getConfig() { return this.config; }
+
+//    public void triggerRunwayEvent() {
+//
+//    }
+
+//    public void triggerAircraftEmergency() {
+//
+//    }
+
+    // getters and setters
+
 }
