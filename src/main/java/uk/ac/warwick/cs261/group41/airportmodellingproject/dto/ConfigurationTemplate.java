fe0882aba8c1f8f49f4b6a91316a0cd74cc14d68
Leroy <leroy.kwarteng@outlook.com>
2026-03-06 12:23:07 +0000

    jsonignore to avoid constructor confusion

--- src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/dto/ConfigurationTemplate.java
+++ src/main/java/uk/ac/warwick/cs261/group41/airportmodellingproject/dto/ConfigurationTemplate.java
@@ -23,7 +23,7 @@ public class ConfigurationTemplate extends SimulationConfig {
     public ConfigurationTemplate() { super(); }
 
     // Constructor used to create a ConfigurationTemplate from an existing SimulationConfig object, used for testing.
-    @JsonIgnore //
+    @JsonIgnore
     public ConfigurationTemplate(String templateName, Date dateCreated, SimulationConfig config) {
         // Pass the data up to the parent's parameterised constructor.
         super(
